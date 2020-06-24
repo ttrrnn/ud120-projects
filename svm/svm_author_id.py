3a1db7f23283c20fc296ce5f09d5dc6ce73faa21
ttrrnn <vtran.sj@gmail.com>
2020-06-23 20:35:15 -0700

    cut down features train to 1%

--- svm/svm_author_id.py
+++ svm/svm_author_id.py
@@ -20,6 +20,10 @@ from email_preprocess import preprocess
 features_train, features_test, labels_train, labels_test = preprocess()
 
 
+# cutting down dataset
+features_train = features_train[:len(features_train)/100] 
+labels_train = labels_train[:len(labels_train)/100] 
+
 from sklearn import svm
 clf = svm.SVC(kernel="linear")
 t0 = time()
