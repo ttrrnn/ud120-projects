e9e4b14cdb3cf7bc1dbb1e87e61ec6cc4fc774df
ttrrnn <vtran.sj@gmail.com>
2020-06-24 14:18:05 -0700

    add code for decesion tree prediction

--- decision_tree/dt_author_id.py
+++ decision_tree/dt_author_id.py
@@ -20,12 +20,11 @@ from email_preprocess import preprocess
 features_train, features_test, labels_train, labels_test = preprocess()
 
 
+from sklearn import tree
+clf = tree.DecisionTreeClassifier(min_samples_split=40)
+clf = clf.fit(features_train, labels_train)
+pred = clf.predict(features_test)
 
-
-#########################################################
-### your code goes here ###
-
-
-#########################################################
-
-
+from sklearn.metrics import accuracy_score
+acc = accuracy_score(labels_test, pred)
+print acc
