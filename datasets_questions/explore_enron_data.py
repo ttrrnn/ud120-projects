85954d7b6708b6b9bd14bb91b4bd5a5817d781dc
ttrrnn <vtran.sj@gmail.com>
2020-06-25 18:01:59 -0700

    added code for calculations

--- datasets_questions/explore_enron_data.py
+++ datasets_questions/explore_enron_data.py
@@ -19,4 +19,35 @@ import pickle
 
 enron_data = pickle.load(open("../final_project/final_project_dataset.pkl", "rb"))
 
+print "total number of datapoints: ", len(enron_data)
+
+
+for ed in enron_data:
+	print "total number of features per datapoints: ", len(enron_data[ed])
+	break
+
+poi_count = 0
+sal = 0
+email = 0
+tp_nan = 0.0
+poi_nan = 0.0
+for ed in enron_data:
+	# print enron_data[ed]
+	if enron_data[ed]["poi"] == 1:
+		poi_count += 1
+		if enron_data[ed]["total_payments"] == "NaN":
+			poi_nan += 1
+	if enron_data[ed]["salary"] != "NaN":
+		sal += 1
+	if enron_data[ed]["email_address"] != "NaN":
+		email += 1
+	if enron_data[ed]["total_payments"] == "NaN":
+		tp_nan += 1
+
+print "total number of POIs: ", poi_count
+print "total number of salary: ", sal
+print "total number of emails: ", email
+print "total number of tp_nan: ", tp_nan
+print "percentage of ppl with tp: ", (tp_nan/len(enron_data))*100
+print "percentage of poi with nan: ", (poi_nan/poi_count)*100
 
