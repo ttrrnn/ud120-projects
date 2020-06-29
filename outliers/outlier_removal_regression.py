a5902d72ee42ab64ad47a19bb0c74fe353ed95ba
ttrrnn <vtran.sj@gmail.com>
2020-06-29 12:26:33 -0700

    added code to print cleaned data values

--- outliers/outlier_removal_regression.py
+++ outliers/outlier_removal_regression.py
@@ -75,6 +75,9 @@ if len(cleaned_data) > 0:
     try:
         reg.fit(ages, net_worths)
         plt.plot(ages, reg.predict(ages), color="blue")
+        print "coef: ", reg.coef_
+        print "intercept: ", reg.intercept_
+        print "score: ", reg.score(ages_test, net_worths_test)
     except NameError:
         print "you don't seem to have regression imported/created,"
         print "   or else your regression object isn't named reg"
