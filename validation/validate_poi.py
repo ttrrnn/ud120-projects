6bdb171c285f90115c2cee8d2f1620da82fad1c2
ttrrnn <vtran.sj@gmail.com>
2020-07-02 13:35:35 -0700

    split data and decision tree

--- validation/validate_poi.py
+++ validation/validate_poi.py
@@ -25,8 +25,13 @@ features_list = ["poi", "salary"]
 data = featureFormat(data_dict, features_list)
 labels, features = targetFeatureSplit(data)
 
+# splitting labels and features for training and testing
+from sklearn.model_selection import train_test_split
+features_train, features_test, labels_train, labels_test = train_test_split(features, labels, test_size=0.30, random_state=42)
 
+# decision tree to classify data
+from sklearn.tree import DecisionTreeClassifier
 
-### it's all yours from here forward!  
-
-
+clf = DecisionTreeClassifier()
+clf.fit(features_train, labels_train)
+print "accuracy: ", clf.score(features_test, labels_test)
