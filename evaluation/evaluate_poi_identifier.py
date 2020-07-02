a1fd621f16dfabeca31a4a69070edd48f8800e24
ttrrnn <vtran.sj@gmail.com>
2020-07-02 15:23:35 -0700

    lesson 15 updates

--- evaluation/evaluate_poi_identifier.py
+++ evaluation/evaluate_poi_identifier.py
@@ -24,8 +24,42 @@ features_list = ["poi", "salary"]
 data = featureFormat(data_dict, features_list)
 labels, features = targetFeatureSplit(data)
 
+# splitting labels and features for training and testing
+from sklearn.model_selection import train_test_split
+features_train, features_test, labels_train, labels_test = train_test_split(features, labels, test_size=0.30, random_state=42)
+
+# decision tree to classify data
+from sklearn.tree import DecisionTreeClassifier
+
+clf = DecisionTreeClassifier()
+clf.fit(features_train, labels_train)
+pred = clf.predict(features_test)
+print "accuracy: ", clf.score(features_test, labels_test)
+
+# count number of POI in test set
+poi_count = 0
+for l in labels_test:
+	if l > 0.0:
+		poi_count += 1
+
+print "no. of POI in test: ", poi_count
+print "total no. of ppl in test: ", len(labels_test)
+
+# confusion matrix visual
+from sklearn.metrics import confusion_matrix, recall_score, precision_score
+print confusion_matrix(labels_test, pred)
+print recall_score(labels_test, pred)
+print precision_score(labels_test, pred)
 
 
-### your code goes here 
+# lesson 15, quiz 35 practice
+true_labels = [0, 0, 0, 0, 0, 0, 1, 0, 1, 1, 0, 1, 0, 1, 1, 1, 0, 1, 0, 0]
+predictions = [0, 1, 1, 0, 0, 0, 1, 0, 1, 0, 0, 1, 0, 0, 1, 1, 0, 1, 0, 1] 
+
+print confusion_matrix(true_labels, predictions)
+print recall_score(true_labels, predictions)
+print precision_score(true_labels, predictions)
+
+
 
 
