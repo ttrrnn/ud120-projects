82e6b7a505ed5259502b26e351911a937db6c4ea
ttrrnn <vtran.sj@gmail.com>
2020-07-03 18:22:02 -0700

    added method to explore dataset with certain features in mind

--- final_project/poi_id.py
+++ final_project/poi_id.py
@@ -1,5 +1,14 @@
 #!/usr/bin/python
 
+'''
+FOR REFERENCE of features
+
+financial_features = ['salary', 'deferral_payments', 'total_payments', 'loan_advances', 'bonus', 'restricted_stock_deferred', 'deferred_income', 'total_stock_value', 'expenses', 'exercised_stock_options', 'other', 'long_term_incentive', 'restricted_stock', 'director_fees']
+
+email_features = ['to_messages', 'email_address', 'from_poi_to_this_person', 'from_messages', 'from_this_person_to_poi', 'shared_receipt_with_poi']
+
+'''
+
 import sys
 import pickle
 sys.path.append("../tools/")
@@ -7,14 +16,63 @@ sys.path.append("../tools/")
 from feature_format import featureFormat, targetFeatureSplit
 from tester import dump_classifier_and_data
 
+def exploreDataSet(data_dict):
+	print "------------------ EXPLORE DATASET --------------------"
+	keys = data_dict.keys()
+	total_data_points = len(data_dict)
+	print "Number of data points: ", total_data_points
+	print "Number of features/person: ", len(data_dict[keys[0]])
+
+	num_poi = 0.0
+	num_nonNaN_salary, num_total_payment, num_bonus, num_long_term_incentive = 0., 0., 0., 0.
+	num_emails_to_poi, num_emails_from_poi = 0., 0.
+
+	for key, val in data_dict.items():
+		if (val['poi'] == 1):
+		    num_poi += 1
+		elif (val['salary'] != 'NaN'):
+		    num_nonNaN_salary += 1
+		elif (val['total_payments'] != 'NaN'):
+			num_total_payment += 1
+		elif (val['bonus'] != 'NaN'):
+			num_bonus += 1
+		elif (val['long_term_incentive'] != 'NaN'):
+			num_long_term_incentive += 1
+		elif (val['from_this_person_to_poi'] != 'NaN'):
+			num_emails_to_poi += 1
+		elif (val['from_poi_to_this_person'] != 'NaN'):
+			num_emails_from_poi +=1
+		else:
+			pass
+
+	print "no. of POI: ", num_poi
+	print "no. of non-POI: ", len(data_dict) - num_poi
+	print "no. of nonNAN salary: ", num_nonNaN_salary, "--", (num_nonNaN_salary/total_data_points)
+	print "no. of total payments: ", num_total_payment, "--", (num_total_payment/total_data_points)
+	print "no. of bonus: ", num_bonus, "--", (num_bonus/total_data_points)
+	print "no. of long term incentive: ", num_long_term_incentive, "--", (num_long_term_incentive/total_data_points)
+	print "no. of emails to POI: ", num_emails_to_poi, "--", (num_emails_to_poi/total_data_points)
+	print "no. of emails from POI: ", num_emails_from_poi, "--", (num_emails_from_poi/total_data_points)
+	print "------------------ END EXPLORE DATASET --------------------"
+
+	# TODO: count how many features are missing and then do some magic
+def exploreFeatures(data_dict):
+	return	# should return features that have NaN values
+
 ### Task 1: Select what features you'll use.
 ### features_list is a list of strings, each of which is a feature name.
 ### The first feature must be "poi".
 features_list = ['poi','salary'] # You will need to use more features
 
+financial_features = ['salary', 'deferral_payments', 'total_payments', 'loan_advances', 'bonus', 'restricted_stock_deferred', 'deferred_income', 'total_stock_value', 'expenses', 'exercised_stock_options', 'other', 'long_term_incentive', 'restricted_stock', 'director_fees']
+
+email_features = ['to_messages', 'email_address', 'from_poi_to_this_person', 'from_messages', 'from_this_person_to_poi', 'shared_receipt_with_poi']
+
 ### Load the dictionary containing the dataset
 with open("final_project_dataset.pkl", "r") as data_file:
     data_dict = pickle.load(data_file)
+
+exploreDataSet(data_dict)
 
 ### Task 2: Remove outliers
 ### Task 3: Create new feature(s)
